9728af8637a5b52d45b42feaec97245891a09f62
Tayon <tayondw@gmail.com>
2025-06-16 23:59:05 -0400

    begin version 2 of project utilizing react-ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Profile from "./components/Profile/Profile";
+
+function App() {
+	return (
+		<div className="min-h-screen">
+			<Profile />
+		</div>
+	);
+}
+
+export default App;
